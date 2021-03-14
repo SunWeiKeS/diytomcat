f82f33a1ec90661d29849b1b187a210318df9c2e
Weisun <1210029395@qq.com>
2021-03-14 19:11:17 +0800

    历史数据更新

--- src/com/sun/diytomcat/FileHistory/Bootstrap/Bootstrap05.java
+++ src/com/sun/diytomcat/FileHistory/Bootstrap/Bootstrap05.java
@@ -1,29 +1,24 @@
-package com.sun.diytomcat;
+package com.sun.diytomcat.FileHistory.Bootstrap;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.log.LogFactory;
 import cn.hutool.system.SystemUtil;
 import com.sun.diytomcat.http.Request;
 import com.sun.diytomcat.http.Response;
 import com.sun.diytomcat.util.Constant;
-import com.sun.diytomcat.util.ThreadPoolUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Bootstrap {
+public class Bootstrap05 {
 
     public static void main(String[] args) {//访问 http://127.0.0.1:18080/
 
@@ -31,53 +26,42 @@ public class Bootstrap {
             logJVM();//像 tomcat 那样 一开始打印 jvm 信息
             int port = 18080;//表示本服务的使用端口号
 
+            //把端口占用提示信息注释掉，当真正异常发生的时候，就会打印出来
+            //if (!NetUtil.isUsableLocalPort(port)) {//用来判断端口是否被占用
+            //    System.out.println(port + " 端口已经被占用了，请关闭本地端口占用");
+            //    return;
+            //}
             //在端口上启动ServerSocket服务，浏览器和服务端通过socket进行通信
             ServerSocket ss = new ServerSocket(port);
 
             while (true) {//处理掉一个Socket链接请求之后，再处理下一个链接请求。
                 //表示收到浏览器客户端的请求
                 Socket s = ss.accept();
-                Runnable r = new Runnable() {
-                    @Override
-                    public void run(){
-                        try {
-                            Request request = new Request(s);
-                            System.out.println("浏览器的输入信息： \r\n" + request.getRequestString());
-                            System.out.println("uri:" + request.getUri());
+                Request request = new Request(s);
+                System.out.println("浏览器的输入信息： \r\n" + request.getRequestString());
+                System.out.println("uri:" + request.getUri());
 
-                            Response response = new Response();
-                            String uri = request.getUri();
-                            if (null == uri)//首先判断 uri 是否为空，如果为空就不处理了。
-                                return;
-                            System.out.println(uri);
-                            if ("/".equals(uri)) {//如果是 "/", 那么依然返回原字符串。
-                                String html = "Hello DIY Tomcat from vicsun";
-                                response.getWriter().println(html);
-                            } else {//接着处理文件，首先取出文件名，比如访问的是 /a.html, 那么文件名就是 a.html
-                                String fileName = StrUtil.removePrefix(uri, "/");
-                                File file = FileUtil.file(Constant.rootFolder, fileName);//然后获取对应的文件对象 file
-                                if (file.exists()) {//如果文件存在，那么获取内容并通过 response.getWriter 打印。
-                                    String fileContent = FileUtil.readUtf8String(file);
-                                    response.getWriter().println(fileContent);
-
-                                    if(fileName.equals("timeConsume.html")){
-                                        //增加对 timeConsume.html 的判断
-                                        ThreadUtil.sleep(1000);
-                                    }
-                                } else {//文件不存在的时候，应该返回404，这里只是返回 File Not Found 字符串。
-                                    response.getWriter().println("File Not Found");
-                                }
-                            }
-                            handle200(s, response);
-                        }catch (IOException e){
-                            e.printStackTrace();
-                        }
+                Response response = new Response();
+                String uri = request.getUri();
+                if (null == uri)//首先判断 uri 是否为空，如果为空就不处理了。
+                    continue;
+                System.out.println(uri);
+                if ("/".equals(uri)) {//如果是 "/", 那么依然返回原字符串。
+                    String html = "Hello DIY Tomcat from vicsun";
+                    response.getWriter().println(html);
+                } else {//接着处理文件，首先取出文件名，比如访问的是 /a.html, 那么文件名就是 a.html
+                    String fileName = StrUtil.removePrefix(uri, "/");
+                    File file = FileUtil.file(Constant.rootFolder, fileName);//然后获取对应的文件对象 file
+                    if (file.exists()) {//如果文件存在，那么获取内容并通过 response.getWriter 打印。
+                        String fileContent = FileUtil.readUtf8String(file);
+                        response.getWriter().println(fileContent);
+                    } else {//文件不存在的时候，应该返回404，这里只是返回 File Not Found 字符串。
+                        response.getWriter().println("File Not Found");
                     }
-                };
-                ThreadPoolUtil.run(r);
+                }
+                handle200(s, response);
             }
         } catch (IOException e) {
-            LogFactory.get().error(e);
             e.printStackTrace();
         }
 
--- src/com/sun/diytomcat/test/TestTomcat.java
+++ src/com/sun/diytomcat/test/TestTomcat.java
@@ -1,5 +1,8 @@
 package com.sun.diytomcat.test;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
@@ -10,6 +13,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Date;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class TestTomcat {
     private static int port=18080;
@@ -39,6 +46,44 @@ public class TestTomcat {
         String html= getContentString("/a.html");
         Assert.assertEquals(html, "Hello DIY Tomcat from a.html");
     }
+
+    @Test
+    public void testTimeConsumeHtml() throws InterruptedException {
+        /**
+         * 因为 Bootstrap 是单线程的，来一个请求，处理一个。 处理完毕之后，才能处理下一个。
+         * 所以我们在单元测试里准备一个线程池，同时模仿3个同时访问 timeConsume.html，
+         * 正是因为 Bootstrap 是单线程的，所以得一个一个地处理，导致3个同时访问，最后累计时间是 3秒以上。
+         *
+         *  准备一个线程池，里面有20根线程。
+         */
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(20, 20, 60, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(10));
+        //开始计时
+        TimeInterval timeInterval = DateUtil.timer();
+
+
+        //连续执行3个任务，可以简单地理解成3个任务同时开始
+        for(int i = 0; i<3; i++){
+            threadPool.execute(new Runnable(){
+                public void run() {
+                    getContentString("/timeConsume.html");
+                }
+            });
+        }
+        //shutdown 尝试关闭线程池，但是如果 线程池里有任务在运行，就不会强制关闭，直到任务都结束了，才关闭.
+        threadPool.shutdown();
+        //awaitTermination 会给线程池1个小时的时间去执行，如果超过1个小时了也会返回，如果在一个小时内任务结束了，就会马上返回。
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+
+        //获取经过了多长时间的毫秒数，并且断言它是超过3秒的。
+        long duration = timeInterval.intervalMs();
+        System.out.println(duration);
+        Assert.assertTrue(duration < 3000);
+    }
+
+
+
+
 
 
     private String getContentString(String uri){
